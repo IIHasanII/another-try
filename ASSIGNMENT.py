1149d24c58e2f0d2230c9aeec61212c0e03ff757
IIHasanII <60573704+IIHasanII@users.noreply.github.com>
2020-02-04 17:43:41 +0500

    Add files via upload

---
+++ ASSIGNMENT.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+print(1)
+
+     
+
+
+# In[4]:
+
+
+print("hasan")
+
+
+# In[7]:
+
+
+a="HASAN"
+b=20
+c="sir syed university"
+print("Name : ", a)
+print("Age : ",b)
+print("University : ",c)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
